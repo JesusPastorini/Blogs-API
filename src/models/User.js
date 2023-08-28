4018deb1b26610349ad4231f2b4171801d70a4d6
JuniorPastorini <juniorpastorini_@hotmail.com>
2023-08-28 05:53:53 -0300

    req 01

---
+++ src/models/User.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    display_name: {
+      type: DataTypes.STRING
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING
+    },
+    image: {
+      type: DataTypes.STRING
+    },
+  }, {
+    underscored: true,
+    tableName: 'users',
+  });
+  return User;
+  };
